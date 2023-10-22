5efbf4e9e2526f7e6d9a40060a772b44dd0b46f9
Mathias Nebiyou <93014669+Monilianmonty@users.noreply.github.com>
2023-10-22 14:12:30 -0400

    Dependencies

---
+++ src/main/java/com/Uni/Unibridges/UnibridgesApplication.java
@@ -0,0 +1,13 @@
+package com.Uni.Unibridges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnibridgesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnibridgesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Uni/Unibridges/UnibridgesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Uni.Unibridges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnibridgesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
