25447efdba88a2077183a360c5f3f91fc8ae23cf
Mathias Nebiyou <93014669+Monilianmonty@users.noreply.github.com>
2023-10-22 20:58:12 -0400

    test

---
+++ src/main/java/com/Uni/Model/Entity/Course.java
@@ -0,0 +1,34 @@
+package com.Uni.Model.Entity;
+
+
+
+
+
+public class Course{
+
+
+
+    private String courseName;
+    private int level;
+
+    public Course(){
+
+    }
+
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
