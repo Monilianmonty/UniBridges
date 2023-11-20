4ffb8d9ab6aa45c22f2dbd98d0169ce4e408630c
Letko <jzl6746@psu.edu>
2023-11-20 18:29:16 -0500

    Hub_view

--- src/main/java/com/Uni/View/Hub.java
+++ src/main/java/com/Uni/View/Hub.java
@@ -6,34 +6,60 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Hub extends JFrame
-{
-Course course[] = new Course[10];
-Student student;
+public class Hub extends JFrame implements ActionListener {
 
-JFrame frame = new JFrame("HUB");
-JButton exitB = new JButton("EXIT HUB");
+    Course course[] = new Course[10]; //set equal to database student
+    Student student; //set equal to database student
 
-public Hub() {
+    JFrame frame = new  JFrame("HUB");
+    JButton exitB, Classes[];
+
+    public Hub()
+    {
+        setTitle("HUB");
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        int x = 20;
 
 
-    ActionListener actionListenerEXIT = new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-
-
-
+        for(int i=0;course[i]!=null;i++) //individual courses
+        {
+            Classes[i] = new JButton("  "); //Text filled with course name in database
+            Classes[i].setBounds(20, 20+x, 50, 20);
+            add(Classes[i]);
+            x+=20;
         }
-    };
-
-
-    exitB.addActionListener(actionListenerEXIT);
 
 
 
+    }
 
-};
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==exitB)
+        {
+            //return to login
+        }
+
+        else {
+            for (int i=0;course[i]!=null;i++)
+            {
+                if(e.getSource()==course[i])
+                {
+                    //send user to chat room
+                }
+            }
+        }
 
 
+
+
+
+    }
 }
+
+
+
