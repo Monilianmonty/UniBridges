c5e83a78571b3cd783fecb7543fc8f2c2948f400
nberg <nbergman679@gmail.com>
2023-11-23 02:48:55 -0500

    1

--- src/main/java/com/Uni/View/CourseChat.java
+++ src/main/java/com/Uni/View/CourseChat.java
@@ -1,75 +1,121 @@
 package com.Uni.View;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CourseChat {
     private JFrame courseFrame;
     private JTextField studentTB;
+    private JTextField usernameTB;
     private JPanel chatArea;
 
     public CourseChat() {
         courseFrame = new JFrame("Course Chat");
         courseFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        courseFrame.setLayout(new BorderLayout());
         courseFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         studentTB = new JTextField();
         studentTB.setPreferredSize(new Dimension(400, 30));
+        studentTB.addKeyListener(new SendMessageListener());
+
+        usernameTB = new JTextField("Username");
+        usernameTB.setPreferredSize(new Dimension(100, 30));
 
         JButton sendMessageButton = new JButton("Send Message");
-        sendMessageButton.addActionListener(new SendMessageListener());
+        sendMessageButton.addActionListener(e -> sendMessage());
 
-        JPanel bottomPanel = new JPanel(new FlowLayout());
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        topPanel.add(usernameTB);
+        topPanel.add(sendMessageButton);
+
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         bottomPanel.add(studentTB);
-        bottomPanel.add(sendMessageButton);
 
         chatArea = new JPanel();
         chatArea.setLayout(new BoxLayout(chatArea, BoxLayout.Y_AXIS));
+        chatArea.setBorder(BorderFactory.createLineBorder(Color.BLACK)); // Visible border
+
         JScrollPane scrollPane = new JScrollPane(chatArea);
 
-        courseFrame.add(scrollPane, BorderLayout.CENTER);
-        courseFrame.add(bottomPanel, BorderLayout.SOUTH);
+        JPanel centerPanel = new JPanel(new BorderLayout());
+        centerPanel.add(scrollPane, BorderLayout.CENTER);
+
+        JPanel controlPanel = new JPanel(new BorderLayout());
+        controlPanel.add(topPanel, BorderLayout.NORTH);
+        controlPanel.add(bottomPanel, BorderLayout.SOUTH);
+
+        courseFrame.add(centerPanel);
+        courseFrame.add(controlPanel, BorderLayout.SOUTH);
 
         courseFrame.setVisible(true);
     }
 
-    // Action listener for sending messages
-    private class SendMessageListener implements ActionListener, KeyListener {
-        public void actionPerformed(ActionEvent e) {
-            sendMessage();
-        }
 
+    // Fetch messages from the database (dummy implementation)
+    private List<String> fetchMessagesFromDatabase() {
+        // INTEGRATE DATABASE MESSAGES HERE
+        List<String> messages = new ArrayList<>();
+        // Example
+        messages.add("Welcome to the chat!");
+
+        return messages;
+    }
+
+    // Action listener for sending messages
+    private class SendMessageListener extends KeyAdapter {
+        @Override
         public void keyPressed(KeyEvent e) {
             if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                 sendMessage();
             }
         }
+    }
 
-        public void keyReleased(KeyEvent e) {}
+    private void sendMessage() {
+        String username = usernameTB.getText();
+        String message = studentTB.getText();
+        if (!message.isEmpty()) {
+            addMessageToChatArea(username, message);
+            studentTB.setText("");
+            // Save message to the database
 
-        public void keyTyped(KeyEvent e) {}
 
-        private void sendMessage() {
-            String message = studentTB.getText();
-            if (!message.isEmpty()) {
-                addMessage(message);
-                studentTB.setText(""); // Clear the text field after sending
-            }
+            saveMessageToDatabase(message);
         }
     }
 
+    // Save message to the database
+    private void saveMessageToDatabase(String message) {
+        // Replace  with database saving logic
+        // Implement inserting the message into your database
+        System.out.println("Saving message to the database: " + message);
+    }
+
     // Method to add messages to the chat area
-    private void addMessage(String message) {
-        JLabel newMessage = new JLabel(message);
-        chatArea.add(newMessage);
-        chatArea.revalidate(); // Refresh the panel to reflect the new message
+    private void addMessageToChatArea(String username, String message) {
+        JPanel messagePanel = new JPanel(new BorderLayout());
+
+        JLabel usernameLabel = new JLabel(username + ": ");
+        usernameLabel.setFont(usernameLabel.getFont().deriveFont(Font.BOLD));
+
+        JLabel messageLabel = new JLabel(message);
+        messageLabel.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
+
+        messagePanel.add(usernameLabel);
+        messagePanel.add(messageLabel);
+       //messagePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        chatArea.add(messagePanel);
+        chatArea.revalidate();
         chatArea.repaint();
     }
+
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new CourseChat());
     }
 }
-
-
---
+++ src/main/java/com/Uni/View/CourseChat2.java
@@ -0,0 +1,160 @@
+//TEST FOR JAVAFX
+
+
+/*
+Would need
+
+        <dependency>
+			<groupId>org.openjfx</groupId>
+			<artifactId>javafx-controls</artifactId>
+			<version>12.0.2</version>
+		</dependency>
+
+
+and
+
+            <plugin>
+				<groupId>org.openjfx</groupId>
+				<artifactId>javafx-maven-plugin</artifactId>
+				<version>0.0.8</version>
+				<configuration>
+					<mainClass>hellofx/org.openjfx.App</mainClass>
+				</configuration>
+			</plugin>
+
+
+in pom.xml file
+ */
+package com.Uni.View;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CourseChat2 extends Application {
+    private TextArea chatArea;
+    private TextField studentTB;
+    private TextField usernameTB;
+    private Connection connection;
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Course Chat");
+
+        BorderPane root = new BorderPane();
+        root.setPadding(new Insets(10));
+
+        HBox topPanel = new HBox(10);
+
+        usernameTB = new TextField("Username");
+        usernameTB.setPrefWidth(100);
+
+        studentTB = new TextField();
+        studentTB.setPrefWidth(400);
+        studentTB.setOnAction(e -> sendMessage());
+
+        Button sendMessageButton = new Button("Send Message");
+        sendMessageButton.setOnAction(e -> sendMessage());
+
+        topPanel.getChildren().addAll(usernameTB, studentTB, sendMessageButton);
+
+        chatArea = new TextArea();
+        chatArea.setEditable(false);
+        chatArea.setWrapText(true);
+
+        ScrollPane scrollPane = new ScrollPane(chatArea);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+
+        root.setCenter(scrollPane);
+        root.setBottom(topPanel);
+
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        initializeDatabase();
+        loadChatHistory();
+    }
+
+    private void initializeDatabase() {
+        try {
+            String jdbcURL = "jdbc:your_database_url_here";
+            String username = "your_username";
+            String password = "your_password";
+            connection = DriverManager.getConnection(jdbcURL, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMessage() {
+        String username = usernameTB.getText();
+        String message = studentTB.getText().trim();
+        if (!message.isEmpty()) {
+            addMessageToChatArea(username, message);
+            saveMessage(username, message);
+            studentTB.clear(); // Clear the text field after sending
+        }
+    }
+
+    private void addMessageToChatArea(String username, String message) {
+        chatArea.appendText(username + ": " + message + "\n");
+    }
+
+    private void saveMessage(String username, String message) {
+        try {
+            String sql = "INSERT INTO chat_history (username, message) VALUES (?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, username);
+            statement.setString(2, message);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadChatHistory() {
+        try {
+            String sql = "SELECT username, message FROM chat_history";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String username = resultSet.getString("username");
+                String message = resultSet.getString("message");
+                addMessageToChatArea(username, message);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void stop() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
